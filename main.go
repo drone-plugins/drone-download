6b60b2c8137ace8351a135dd09835c39a2248a3e
TP Honey <tp@harness.io>
2022-12-07 11:44:12 +0000

    (maint) cleaning build up with move to harness

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ func main() {
 	settings := &plugin.Settings{}
 
 	if _, err := os.Stat("/run/drone/env"); err == nil {
-		godotenv.Overload("/run/drone/env")
+		_ = godotenv.Overload("/run/drone/env")
 	}
 
 	app := &cli.App{
@@ -66,5 +66,52 @@ func run(settings *plugin.Settings) cli.ActionFunc {
 		}
 
 		return nil
+	}
+}
+
+func settingsFlags(settings *plugin.Settings) []cli.Flag {
+	return []cli.Flag{
+		&cli.StringFlag{
+			Name:        "source",
+			Usage:       "source url for the download",
+			EnvVars:     []string{"PLUGIN_SOURCE"},
+			Destination: &settings.Source,
+		},
+		&cli.StringFlag{
+			Name:        "destination",
+			Usage:       "destination for the download",
+			EnvVars:     []string{"PLUGIN_DESTINATION"},
+			Destination: &settings.Destination,
+		},
+		&cli.StringFlag{
+			Name:        "authorization",
+			Usage:       "value to send in the authorization header",
+			EnvVars:     []string{"PLUGIN_AUTHORIZATION", "DOWNLOAD_AUTHORIZATION"},
+			Destination: &settings.Authorization,
+		},
+		&cli.StringFlag{
+			Name:        "username",
+			Usage:       "username for basic auth",
+			EnvVars:     []string{"PLUGIN_USERNAME", "DOWNLOAD_USERNAME"},
+			Destination: &settings.Username,
+		},
+		&cli.StringFlag{
+			Name:        "password",
+			Usage:       "password for basic auth",
+			EnvVars:     []string{"PLUGIN_PASSWORD", "DOWNLOAD_PASSWORD"},
+			Destination: &settings.Password,
+		},
+		&cli.StringFlag{
+			Name:        "md5-checksum",
+			Usage:       "checksum in md5 format",
+			EnvVars:     []string{"PLUGIN_MD5"},
+			Destination: &settings.MD5,
+		},
+		&cli.StringFlag{
+			Name:        "sha256-checksum",
+			Usage:       "checksum in sha256 format",
+			EnvVars:     []string{"PLUGIN_SHA256", "PLUGIN_SHA265"},
+			Destination: &settings.SHA256,
+		},
 	}
 }
